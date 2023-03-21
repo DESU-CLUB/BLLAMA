59ab390f5bf4f3080b2d4811d1916d05564787bc
DESU-CLUB <warrenlowkh@gmail.com>
2023-03-22 01:10:52 +0800

    Removed some debugging code

--- generate.py
+++ generate.py
@@ -83,7 +83,6 @@ def generate_prompt(instruction, input=None, context = None):
 ### Response:"""
     elif context:
          print('Context only mode')
-         print(context)
          return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
 ### Instruction:
